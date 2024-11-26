1572e2f29f3cbfe6c79c1a89beb0adac802bf1a3
Sunitha <sunithab188@gmail.com>
2024-11-26 22:29:05 +0530

    Products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -191,7 +191,7 @@ console.log(totalPages)
               <div
                 className={`inline-grid gap-x-3 gap-y-1 mx-auto md:gap-3 
                   ${filteredProducts?.length === 1 ? "grid-cols-1" : ""} 
-                  ${filteredProducts?.length === 2 ? "md:grid-cols-3 grid-cols-2" : "grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4"} 
+                  ${filteredProducts?.length === 2 ? "md:grid-cols-4 grid-cols-2" : "grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4"} 
                   w-full`}
               >
                 {isLoading ? (
