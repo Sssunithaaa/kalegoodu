d9cc0169852aca110b21a9abb85e0e33f8b8e6de
Sunitha <sunithab188@gmail.com>
2025-01-14 16:17:38 +0530

    SearchBar

--- src/searchbar/SearchBar.jsx
+++ src/searchbar/SearchBar.jsx
@@ -3,12 +3,12 @@ import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-import { getAllProducts } from '../services/index/products';
+import { getAllProducts, getProductNames } from '../services/index/products';
 const SearchBar = ({ isSearchBarVisible, toggleSearchbar }) => {
    const baseUrl = import.meta.env.VITE_APP_URL
    const {data=[],isLoading,isFetching}=useQuery({
-    queryKey:["products"],
-    queryFn: getAllProducts
+    queryKey:["testimonial-products"],
+    queryFn: getProductNames
    }) 
 
   const handleOnSearch = (string, results) => {
@@ -31,23 +31,28 @@ const SearchBar = ({ isSearchBarVisible, toggleSearchbar }) => {
 
   };
 
-  const formatResult = (item) => {
-    return (
-      <>
-     <div className='flex flex-row z-[1000] h-[100%] gap-x-4'>
-      <img src={"https://res.cloudinary.com/dgkgxokru/"+item?.images?.[0]?.image} alt={item.name} style={{ width: '50px',height:'50px'}}/>
-      <div>
-                <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
-        <span style={{ display: 'block', textAlign: 'left' }}>Rs. {item.price}</span>
+const formatResult = (item) => {
+  return (
+    <>
+      <div className="flex flex-row z-[1000] h-[100%] gap-x-4 items-center">
+        <img
+          src={item?.images?.[0]?.image_url} // Use the full URL directly
+          alt={item.name}
+          style={{ width: "50px", height: "50px", objectFit: "cover" }}
+        />
+        <div>
+          <span style={{ display: "block", textAlign: "left" }}>{item.name}</span>
+          <span style={{ display: "block", textAlign: "left" }}>Rs. {item.discounted_price !== 0 ? item.discounted_price : item.price}</span>
+        </div>
       </div>
-     </div>
-      </>
-    );
-  };
+    </>
+  );
+};
+
   if(!isSearchBarVisible){
     return null;
   }
- 
+   console.log(data)
   return (
     <div className=' top-0 w-[80%] z-[1000001] md:w-[50%]' style={{ position: 'relative',marginInline:'auto',marginBlock:'10px',borderRadius:'0px' }}>
    <ReactSearchAutocomplete
